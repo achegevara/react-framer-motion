d79d706088ed0ab70e5976ab2e18840b28772fb9
achegevara <charniauski.aliaksei@gmail.com>
2024-09-15 20:49:07 +0300

    Add project code to repo.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import WelcomePage from './pages/Welcome.jsx';
+import ChallengesPage from './pages/Challenges.jsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <WelcomePage /> },
+  { path: '/challenges', element: <ChallengesPage /> },
+  { path: '*', element: <WelcomePage /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
